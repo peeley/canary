944466557a65ccb2d8caedb386e583f56d67cbf3
Bodo <noah.snelson@protonmail.com>
2018-01-21 23:55:35 -0900

    finalized state size of chain, also finalized NL processing

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import pandas as pd
-# import matplotlib
-# from  matplotlib import pyplot as plt
 import markovify
 import spacy
-
+import time
 
 # adds natural language processing to tweet generation
 nlp = spacy.load('en')
+
 class POSifiedText(markovify.Text):
     def word_split(self, sentence):
         return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
@@ -28,13 +27,14 @@ for i in text:
 final_model = markovify.combine(models=tweet_models)
 
 # generates tweet, refuses 'None' content
-def output(model):
+def generate_tweet(model):
 	tweet = str(model.make_sentence())
-	if(tweet == 'None ' or tweet == 'None'):
-		return(output(model))
+	if(tweet[:5] == 'None'):
+		return(generate_tweet(model))
 	else:
 		return tweet
 
 # outputs to CLI
-for i in range(0, 10):	
-	print(str(output(final_model)) + '\n')
+while(True):
+	print(str(generate_tweet(final_model)) + '\n')
+	time.sleep(2)
